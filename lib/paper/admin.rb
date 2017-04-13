fa63a933fa7114720ac4e75af551ee749a75e788
KhoaRB <ThanhKhoaIT@gmail.com>
2017-04-14 01:32:56 +0700

    Init gem

---
+++ lib/paper/admin.rb
@@ -0,0 +1,7 @@
+require "paper/admin/version"
+
+module Paper
+  module Admin
+    # Your code goes here...
+  end
+end
---
+++ lib/paper/admin/version.rb
@@ -0,0 +1,5 @@
+module Paper
+  module Admin
+    VERSION = "0.1.0"
+  end
+end
